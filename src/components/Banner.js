9e6e220bd9f7321902ef31005f3a751ecd3bd80f
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-07-01 10:54:25 +0530

    Banner image from API added 😎

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Banner.module.css";
+import requests from "../api/requests";
+import axios from "../api/axios";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   };
@@ -11,7 +29,7 @@ function Banner() {
       className={classes.banner}
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png)`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
