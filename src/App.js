1364e495cc9a6f2bb03a9e61eb568c2270edbc61
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-07-01 15:11:46 +0530

    added router to the app ☺

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import HomeScreen from "./components/HomeScreen";
 import classes from "./App.module.css";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className={classes.app}>
-      <HomeScreen />
+      <Switch>
+        <Route path="/" exact>
+          <HomeScreen />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </div>
   );
 }
